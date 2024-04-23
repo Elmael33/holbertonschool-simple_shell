a4e630ed6d701103aec1353c5c4cb26220c63e3b
Elmael33 <mael.salet33@gmail.com>
2024-04-23 15:11:28 +0200

    fix print error message, and the line where is the error

--- execute.c
+++ execute.c
@@ -19,12 +19,15 @@ void execute_command(char **argv)
 	}
 	else if (pid == 0)
 	{
-		if (isatty(STDIN_FILENO) - 1)
+		if (isatty(STDIN_FILENO) == 0)
 		{
-			fprintf(stderr, "not found %s\n", argv[0]);
-			exit(1);
+			if (pid == '\0')
+			{
+				int i = 1;
+				fprintf(stderr, "%d : %s : not found\n", i, argv[0]);
+				exit(1);
+			}
 		}
-		
 		if (execve(argv[0], argv, environ) == -1)
 		{
 			perror("./hsh");
