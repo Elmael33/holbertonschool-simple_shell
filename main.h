623cf6dcf396452f30b6a54306fcbd8e7b1c098b
SofianeSlimane <sofianeslimane24@gmail.com>
2024-04-24 20:18:03 +0000

    fixed Betty main.h

--- main.h
+++ main.h
@@ -5,7 +5,7 @@
 #include <unistd.h>
 #include <string.h>
 #include <sys/wait.h>
-extern char** environ;
+extern char **environ;
 void prompt(void);
 char **read_line(int flag, char *buffer);
 void execute_command(char **argv);
